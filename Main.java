78b92975f42b263cc8c66a646a28fef7e8b66879
Varga445 <51417234+Varga445@users.noreply.github.com>
2020-05-04 22:48:39 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,10 +8,5 @@ public class Main {
         studentDataBase.sudentDate();
         System.out.println("------------------------------------------");
 
-//        ClasaA a1 = new ClasaA("Dave", "Smith");
-//        System.out.println(a1);
-//        System.out.println("I didn't use  \"toString()\" for the Student Obj because " + "\n" +
-//                "I've wanted a project a bit different." + "\n" + "I've created a random Class with " +
-//                "just to use that method ");
     }
 }
